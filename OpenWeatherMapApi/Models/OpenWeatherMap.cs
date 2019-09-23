79d7e5e8c01461bfa0cc242e353c46336d9627a0
Hemant Deshmukh <hdeshmukh@MERILENT.COM>
2019-09-23 08:35:12 -0400

    Push Changes

---
+++ OpenWeatherMapApi/Models/OpenWeatherMap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OpenWeatherMapApi.Models
+{
+    public class OpenWeatherMap
+    {
+        public string apiResponse { get; set; }
+
+        public Dictionary<string, string> cities
+        {
+            get; set;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # OpenWetherMapApi
-To get Wether Information
+This Project Get the Information Weather Of Country Base On Country Id 
+Technology  Dot net MVC  Version 4.6.1 
+I did not used here File to get in information of country with Conutry Id i have used Dictionary.
+
